4ba5aa61e875ecbec991a0ce9f8b19835e87a2dd
sbal13 <sbal13@gmail.com>
2018-02-07 13:20:55 -0500

    changed ternary into binary conditional

--- test/indexTest.js
+++ test/indexTest.js
@@ -242,7 +242,6 @@ describe('index.js', function () {
 
     it('removes duplicate values from an array when an iteratee is applied', function () {
       const newArr = fi.uniq([1, 2, 2, 3, 4, 6, 9], false, (val => val % 3))
-      console.log(newArr)
       expect(arraysEqual(newArr, [1, 2, 3])).to.equal(true)
     })
 
